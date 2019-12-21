5678de15d175b879795cbfc8c854fc68f7baee8f
Anton Odintsov <anton@oneclickdev.com>
2019-12-21 15:46:48 +0300

    dockerContainersRunning

--- src/Client.php
+++ src/Client.php
@@ -86,6 +86,13 @@ class Client
 
         return $info;
     }
+    
+    public function dockerContainersRunning(int $endpointId): array
+    {
+        $info = $this->client->request('GET', "endpoints/{$endpointId}/docker/containers/json?all=0", [], $this->client->session()->headers);
+
+        return $info;
+    }
 
     /**
      * Docker stacks API
